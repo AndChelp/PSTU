a8f732b0d31cae3c3f44d98ec6cd344005eec135
AndChelp <AndChelp@yandex.ru>
2020-12-16 17:25:18 +0500

    Найти сумму ряда с точностью e=10-4

---
+++ lab2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    double eps = pow(10, -4);
+    int n = 1;
+    double sum = 0;
+    double a;
+    do {
+        a = 1 / pow(2, n) + 1 / pow(3, n);
+        sum += a;
+        n++;
+    } while (a > eps);
+    std::cout << sum;
+}
