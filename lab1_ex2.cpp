a99633f253184e0bb19f2a764698302ad18e34a9
Chelpanov Andrey <chelpanov-aa@itrev.ru>
2020-12-18 17:40:39 +0500

    Вычислить значения выражений.

--- lab1_ex2.cpp
+++ lab1_ex2.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 using namespace std;
+
 int main() {
-    int m, n = 10;
+    int m = 10, n = 10;
     cout << m - ++n << "\n";
     m = n = 10;
     cout << (++m > --n) << "\n";
