f662367eb92d335197ec493b2bc5eecce52cc143
AndChelp <AndChelp@yandex.ru>
2020-12-22 16:39:43 +0500

    6. 	Преобразовать строку таким образом, чтобы сначала в ней были напечатаны только буквы, а потом только циф-ры, не меняя порядка следования символов в строке.

---
+++ lab6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+
+void shift(char *string, int from, int size) {
+    char number = string[from];
+    while (from++ < size) {
+        string[from - 1] = string[from];
+    }
+    string[size] = number;
+}
+
+int main() {
+    char str[256];
+    gets(str);
+    int strLength = strlen(str);
+    int uncheckedLength = strLength;
+    for (int i = 0; i < uncheckedLength; ++i) {
+        if (str[i] >= 48 && str[i] <= 57) {
+            uncheckedLength--;
+            shift(str, i--, strLength - 1);
+        }
+    }
+    std::cout << str;
+}
