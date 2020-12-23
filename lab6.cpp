d341f7cc6c5e7a7dfaa7c252b62398dcd8a7ce13
Chelpanov Andrey <chelpanov-aa@itrev.ru>
2020-12-23 16:43:48 +0500

    Преобразовать строку таким образом, чтобы сначала в ней были напечатаны только буквы, а потом только цифры, не меняя порядка следования символов в строке.

--- lab6.cpp
+++ lab6.cpp
@@ -12,6 +12,7 @@ int main() {
     char str[256];
     std::cout << "Строка: ";
     gets_s(str);
+    //gets(str);
     int strLength = strlen(str);
     int uncheckedLength = strLength;
     for (int i = 0; i < uncheckedLength; ++i) {
