bed1cc851e1eed1bcc01cdb6f1502a660a7016a9
Chelpanov Andrey <chelpanov-aa@itrev.ru>
2021-01-08 15:14:53 +0500

    7

--- lab7.cpp
+++ lab7.cpp
@@ -28,12 +28,16 @@ void fillRandom(T *array, int size) {
     printArray(array, size);
 }
 
+void _1_defaultParams(const string &surname = "Фамилия", const string &address = "Адрес", short age = 20) {
+    cout << surname << ", " << address << ", " << age << "\n";
+}
+
 void _2_findOddMax(int n, ...) {
     int *p = &n;
-    int index = 0;
+    int pos = 0;
     int max = *(++p);
     for (; n > 0; n--) {
-        if (index++ % 2 != 0 && max < *(p)) {
+        if (pos++ % 2 != 0 && max < *(p)) {
             max = *(p);
         }
         p++;
@@ -77,10 +81,40 @@ void _3_arraySort(double *array, int size) {
     _3_abstractArraySort(array, size);
 }
 
+double _4_equation(double x) {
+    return 0.25 * pow(x, 3) + x - 1.2502;
+}
+
+void _4_solveEquation(double (*eq)(double)) {
+    double a = 0, b = 2;
+    double e = 1.0001;
+    double c;
+    do {
+        c = (a + b) / 2;
+        if (((*eq)(a) * (*eq)(c)) < 0) {
+            b = c;
+        } else {
+            a = c;
+        }
+    } while (fabs(b - a) > e);
+
+    cout << c;
+}
+
+
 int main() {
+    cout << "1)\n";
+    cout << "Явные параметры: ";
+    _1_defaultParams("ф", "а", 0);
+    cout << "Дефолтные параметры: ";
+    _1_defaultParams();
+    cout << "Частичные параметры: ";
+    _1_defaultParams("ф");
     cout << "-----------------------------------\n";
-    cout << "2) ";
-    _2_findOddMax(11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10); //нечетные позиции с учетом нулевого индекса
+    cout << "2)\n"
+         << "Максимальное число на нечетной позиции\n"
+         << "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]: ";
+    _2_findOddMax(11, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     cout << "-----------------------------------\n";
     cout << "3) ";
     int size = intInput("Размер массива: ");
@@ -90,19 +124,23 @@ int main() {
     fillRandom(intArray, size);
     _3_arraySort(intArray, size);
     printArray(intArray, size);
+    delete[] intArray;
 
     cout << "Char: \n";
     char *charArray = new char[size];
     fillRandom(charArray, size);
     _3_arraySort(charArray, size);
     printArray(charArray, size);
+    delete[] charArray;
 
     cout << "Double: \n";
     double *doubleArray = new double[size];
     fillRandom(doubleArray, size);
     _3_arraySort(doubleArray, size);
     printArray(doubleArray, size);
+    delete[] doubleArray;
     cout << "-----------------------------------\n";
-    cout << "4) ";
+    cout << "4)\nКорень уравнения: ";
 
+    _4_solveEquation(&_4_equation);
 }
